5564fa3b19df9d8c6f776109a2452d2ac552b32d
yoyo <hsjejxys@gmail.com>
2024-03-16 15:12:49 +0900

    synchronous.jsに書き換え

--- asynchronous/AsyncAwait.js
+++
@@ -1,9 +0,0 @@
-// 同期処理を行う関数を宣言
-const synchronousFunc = () => {
-    console.log("これは同期処理関数内のログです");
-    return '完了!'
-};
-
-const message = '同期処理'
-const result = synchronousFunc()
-console.log(message + result)
