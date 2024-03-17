8aebce336c14474af8e986f2f74d4f0d9fba8838
yoyo <hsjejxys@gmail.com>
2024-03-17 13:44:18 +0900

    import export

---
+++ simabu/normal/foo.js
@@ -0,0 +1,19 @@
+let youtuber = {
+    list: {
+        bussiness: {
+            youtuber: [
+                { name: "しまぶー", age: 20, teachProgramming() { } },
+                { name: "あっちゃん", age: 37, teachProgramming() { } }
+            ],
+            teach() { }
+        },
+        entertainment: {
+            youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
+            makeSmile() { }
+        },
+    },
+    plan() { },
+    uoloadvideo() { },
+};
+
+export default youtuber;
--- simabu/normal/index.js
+++ simabu/normal/index.js
@@ -1,23 +1,5 @@
-let youtuber = {
-    list: {
-        bussiness: {
-            youtuber: [
-                { name: "しまぶー", age: 20, teachProgramming() { } },
-                { name: "あっちゃん", age: 37, teachProgramming() { } }
-            ],
-            teach() { }
-        },
-        entertainment: {
-            youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
-            makeSmile() { }
-        },
-    },
-    plan() { },
-    uoloadvideo() { },
-};
-
-youtuber.list.bussiness.youtuber[0].name
+import youtuber from "./foo";
 
 youtuber.plan()
 
-youtuber.list.entertainment.makeSmile()
+youtuber.list.bussiness.youtuber[0].name
