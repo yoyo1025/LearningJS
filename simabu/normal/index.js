f10baa18a35586dcd902734970208d6808d85d21
yoyo <hsjejxys@gmail.com>
2024-03-17 13:56:44 +0900

    consoleやdocument

--- simabu/normal/index.js
+++ simabu/normal/index.js
@@ -3,3 +3,8 @@ import youtuber from "./foo";
 youtuber.plan()
 
 youtuber.list.bussiness.youtuber[0].name
+
+window.console.log(); // windowが無くてもいい
+window.alert();
+
+window.document.getElementById();
