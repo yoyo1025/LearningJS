048a664f415dfd886230d5db60195a480c1be08c
yoyo <hsjejxys@gmail.com>
2024-03-17 14:08:08 +0900

    変数とは

--- simabu/normal/index.js
+++ simabu/normal/index.js
@@ -8,3 +8,18 @@ window.console.log(); // windowが無くてもいい
 window.alert();
 
 window.document.getElementById();
+
+
+/**
+ * Q. 変数とは？
+ * A. データを保存しておくための箱。
+ */
+const fooWidth = document.getElementById("foo").offsetWidth;
+
+const fooHeight = document.getElementById("foo").offsetHeight;
+
+// いったん変数に格納してから縦・横ごとにメソッドを付けてもいい
+const foo = document.getElementById("foo");
+const fooWidth2 = foo.offsetWidth;
+const fooHeight2 = foo.offsetHeight;
+const fooArea = fooWidth2 * fooHeight2;
